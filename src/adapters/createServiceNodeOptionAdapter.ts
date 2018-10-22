e09a5cf8aad95af2c5cb93c9dd0c9d4134f9d87f
nix <qiuwei@huobi.com>
2018-10-22 19:16:05 +0800

    feature: apply health data, map to color

--- src/adapters/createServiceNodeOptionAdapter.ts
+++ src/adapters/createServiceNodeOptionAdapter.ts
@@ -1,13 +1,24 @@
 import { ServiceNodeOption, Node } from '../../typings/defines';
+import { HEALTH } from '../HEALTH';
+
+interface HealthColor {
+  [key: string]: string;
+}
+
+const HEALTH_COLOR: HealthColor = {
+  [HEALTH.HEALTHY]: '#a9d86e',
+  [HEALTH.NORMAL]: '#86cae4',
+  [HEALTH.INTOLERANCE]: '#f58210',
+}
 
 export default function createServiceNodeOption(node: Node): ServiceNodeOption {
   return { 
     title: node.showName,
     instances: `${node.activeInstances}/${node.instances}`,
-    color: 'red',
+    color: HEALTH_COLOR[node.health],
     className: `${node.type} node`,
     type: node.type,
-    avgRT: 0,
+    avgRT: node.elapsedTime,
     rpm: node.rpm,
     epm: node.epm,
     id: node.id,

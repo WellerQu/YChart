b533aa0c2cd9d27823b6b334bd0598795b78ca95
nix <qiuwei@huobi.com>
2018-10-25 14:26:45 +0800

    optimize

--- build/webpack.production.config.js
+++ build/webpack.production.config.js
@@ -1,11 +1,23 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
+
 const ManifestPlugin = require('webpack-manifest-plugin');
+const BannerPlugin = webpack.BannerPlugin;
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const baseConfig = require('./webpack.base.config');
+const pkg = require('../package.json');
 
 module.exports = merge(baseConfig, {
   devtool: false,
   plugins: [
+    new BannerPlugin({
+      banner: `Y chart engine version:${pkg.version} from: https://github.com/WellerQu/YChart`,
+      entryOnly: true,
+    }),
     new ManifestPlugin(),
+    // new UglifyJsPlugin({
+    //   cache: true,
+    // }),
   ],
 });
